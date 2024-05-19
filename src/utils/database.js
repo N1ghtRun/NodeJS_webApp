7910e12fb5e18ebb278cc8a140e2d5abc866656c
N1ghtRun <dmitro.shpanchuck@gmail.com>
2024-05-19 16:22:55 +0300

    added dependencies

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/eventsApp';
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://admin:AKsOtZCXH7y7ME61@cluster0.7q0llt6.mongodb.net/eventsApp';
 
 const connectDB = async () => {
   try {
